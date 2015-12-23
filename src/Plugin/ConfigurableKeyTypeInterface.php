cd03db34e1b97d9b897ef3f3595930038b3d56e6
Rick Hawkins <rick@alchemydes.com>
2015-12-22 16:38:52 -0800

    Fixed ConfigurableKeyTypeInterface.

--- src/Plugin/ConfigurableKeyTypeInterface.php
+++ src/Plugin/ConfigurableKeyTypeInterface.php
@@ -7,33 +7,11 @@
 
 namespace Drupal\key\Plugin;
 
-use Drupal\Component\Plugin\PluginInspectionInterface;
-use Drupal\key\KeyInterface;
+use Drupal\Component\Plugin\ConfigurablePluginInterface;
+use Drupal\Core\Plugin\PluginFormInterface;
 
 /**
  * Provides an interface for configurable Key Type plugins.
  */
-interface ConfigurableKeyTypeInterface extends PluginInspectionInterface {
-
-  /**
-   * Allows the Key Type plugin to generate a key value.
-   *
-   * @param \Drupal\key\KeyInterface $key
-   *   The key entity object.
-   *
-   * @return string
-   *   The generated key value.
-   */
-  public function generateKeyValue(KeyInterface $key);
-
-  /**
-   * Allows the Key Type plugin to validate the key value.
-   *
-   * @param string $key_value
-   *   The key value
-   * @param \Drupal\key\KeyInterface $key
-   *   The key entity object.
-   */
-  public function validateKeyValue($key_value, KeyInterface $key);
-
+interface ConfigurableKeyTypeInterface extends KeyTypeInterface, ConfigurablePluginInterface, PluginFormInterface {
 }
