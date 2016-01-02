24bc810e5b8ff9dd301d67e0f4535f7442cc0ddf
rlhawk <rlhawk@352283.no-reply.drupal.org>
2016-01-02 14:33:44 -0800

    Issue #2643420 by rlhawk: Create a Text Field key input plugin

---
+++ src/Plugin/KeyInput/TextFieldKeyInput.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\key\Plugin\KeyInput\TextFieldKeyInput.
+ */
+
+namespace Drupal\key\Plugin\KeyInput;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\key\Plugin\KeyInputBase;
+
+/**
+ * Defines a key input that provides a simple text field.
+ *
+ * @KeyInput(
+ *   id = "text_field",
+ *   label = @Translation("Text field"),
+ *   description = @Translation("A simple text field.")
+ * )
+ */
+class TextFieldKeyInput extends KeyInputBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['key_value'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Key value',
+      '#required' => TRUE,
+      '#default_value' => $this->getConfiguration()['key_value'],
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processSubmittedKeyValue(array &$form, FormStateInterface $form_state) {
+    return '';
+  }
+
+}
