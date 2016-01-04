26298c0f0b6c6f10beec01d543ec63a15e9b690f
rlhawk <rlhawk@352283.no-reply.drupal.org>
2016-01-04 12:13:46 -0800

    Use entity label in title of page when editing key.

--- src/Form/KeyEditForm.php
+++ src/Form/KeyEditForm.php
@@ -19,6 +19,14 @@ class KeyEditForm extends KeyFormBase {
   /**
    * {@inheritdoc}
    */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form['#title'] = $this->t('Edit key %label', array('%label' => $this->entity->label()));
+    return parent::form($form, $form_state);
+  }
+
+    /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
     drupal_set_message($this->t('Key %name has been updated.', array('%name' => $this->entity->label())));
