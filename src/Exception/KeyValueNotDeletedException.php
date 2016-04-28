8d700487a2cb6d3083c1f9bd4fde0bd524cc604b
rlhawk <rlhawk@352283.no-reply.drupal.org>
2016-04-28 11:52:23 -0700

    Issue #2696281 by rlhawk: Add Key-specific exceptions

---
+++ src/Exception/KeyValueNotDeletedException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\key\Exception\KeyValueNotDeletedException.
+ */
+
+namespace Drupal\key\Exception;
+
+/**
+ * Defines an exception for when a key value fails to be deleted.
+ */
+class KeyValueNotDeletedException extends KeyException { }
---
+++ src/Exception/KeyValueNotRetrievedException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\key\Exception\KeyValueNotRetrievedException.
+ */
+
+namespace Drupal\key\Exception;
+
+/**
+ * Defines an exception for when a key value fails to be retrieved.
+ */
+class KeyValueNotRetrievedException extends KeyException { }
